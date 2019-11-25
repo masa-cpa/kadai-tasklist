ca23639155b6c3f1edd5958d0b40b596e8fe2cf8
masafumi.nishimura <nishimura.masafumi@nishimura-cpa.jp>
2019-11-25 11:40:44 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  root to: 'sessions#new'
+#  get 'sessions/new'
+#  get 'sessions/create'
+#  get 'sessions/destroy'
+
+  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
