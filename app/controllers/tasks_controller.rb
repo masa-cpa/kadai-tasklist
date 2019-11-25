7072920979a0f63f9fcd3c9f61595622fee37779
masafumi.nishimura <nishimura.masafumi@nishimura-cpa.jp>
2019-11-25 12:31:09 +0000

    post 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,18 +3,15 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
     
   def index
-    if logged_in?
 #    @tasks = Task.all
-      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc)
-    end
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
@@ -52,12 +49,18 @@ class TasksController < ApplicationController
   private
   #DRY
   def set_task
-    @task = Task.find(params[:id])
+#    @task = Task.find(params[:id]) →全てのタスクからこのIDを見つけてくる
+# => 下記はカレントユーザーが作成したタスクのなかからIDを見つけてくる
+    @task = currrnt.user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
     
   #strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
 
 end
